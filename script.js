bd14a6c0bd95e2028f720daaffb5d27e2016eef7
Kácio <63382277+Oicak@users.noreply.github.com>
2023-05-22 11:02:27 -0300

    gif

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function playAudio() {
     x.play()
     document.getElementById('sec').style.display='none'
     document.getElementById('sec1').style.display='inline-block'
-    document.getElementsByTagName('body')[0].style.background='#F8F6F2'
+    document.getElementsByTagName('body')[0].style.backgroundImage='url(background.gif)'
 }    
