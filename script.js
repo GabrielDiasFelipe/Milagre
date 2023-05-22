3d541ca25c175dd2faa1032087ca1799ac3ab840
Kácio <63382277+Oicak@users.noreply.github.com>
2023-05-22 10:53:37 -0300

    milagre site

---
+++ script.js
@@ -0,0 +1,7 @@
+function playAudio() {
+    let x = document.getElementById("myAudio")
+    x.play()
+    document.getElementById('sec').style.display='none'
+    document.getElementById('sec1').style.display='inline-block'
+    document.getElementsByTagName('body')[0].style.background='#F8F6F2'
+}    
