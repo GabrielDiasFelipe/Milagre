6765aaaa9e905c0b81c50a89a033f91ad9d80287
Kácio <63382277+Oicak@users.noreply.github.com>
2023-05-22 11:19:29 -0300

    fundo animado

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function playAudio() {
     let x = document.getElementById("myAudio")
     x.play()
     document.getElementById('sec').style.display='none'
+    document.getElementsByTagName('section')[0].style.display='none'
     document.getElementById('sec1').style.display='inline-block'
-    document.getElementsByTagName('body')[0].style.backgroundImage='url(background.gif)'
 }    
